4980a8c3e015e7ffd728e8eddca488f92f8b898b
Marco Gordillo <marcopgordillo@gmail.com>
2018-05-30 17:50:48 -0500

    added repositories

--- src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
@@ -25,6 +25,9 @@ public class Recipe {
     private Notes notes;
 
     @ManyToMany
+    @JoinTable(name = "recipe_category",
+        joinColumns = @JoinColumn(name = "recipe_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
