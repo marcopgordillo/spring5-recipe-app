d924cb0207e25924420237cfd0f748f4fde62982
Marco Gordillo <marcopgordillo@gmail.com>
2018-06-08 17:46:03 -0500

    added Bad Request Exception

--- src/test/java/guru/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -67,6 +67,19 @@ public class RecipeControllerTest {
     }
 
     @Test
+    public void testGetRecipeNumberFormat() throws Exception {
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        //when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
+
+        mockMvc.perform(get("/recipe/asdf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
+
+    @Test
     public void testGetNewRecipeForm() throws Exception {
         RecipeCommand command = new RecipeCommand();
 
